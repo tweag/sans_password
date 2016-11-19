a415e2145c00315a3a332a244af9319257dcead0
Ray Zane <ray@promptworks.com>
2016-11-19 14:26:17 -0500

    Add a macro for schema declaration

--- lib/passwordless/schema.ex
+++ lib/passwordless/schema.ex
@@ -1,12 +1,22 @@
 defmodule Passwordless.Schema do
   defmacro __using__(_) do
     quote do
+      import Passwordless.Schema
+
       @login_fields ~w(login_token login_requested_at last_login_at)
 
       def passwordless_changeset(struct, params \\ %{}) do
         struct
         |> Ecto.Changeset.cast(params, @login_fields)
       end
+    end
+  end
+
+  defmacro passwordless_schema() do
+    quote do
+      field :login_token, :string
+      field :login_requested_at, Ecto.DateTime
+      field :last_login_at, Ecto.DateTime
     end
   end
 end
--- test/support/test_user.ex
+++ test/support/test_user.ex
@@ -4,8 +4,6 @@ defmodule Passwordless.TestUser do
 
   schema "users" do
     field :email, :string
-    field :login_token, :string
-    field :login_requested_at, Ecto.DateTime
-    field :last_login_at, Ecto.DateTime
+    passwordless_schema()
   end
 end
