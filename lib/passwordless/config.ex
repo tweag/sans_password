bea5db75b9e024aa2fd1ff4af4a204bb24a833bb
Ray Zane <ray@promptworks.com>
2016-11-21 21:02:51 -0500

    Avoid missing config

--- lib/passwordless/config.ex
+++ lib/passwordless/config.ex
@@ -1,18 +1,20 @@
 defmodule Passwordless.Config do
+  @config Application.get_env(:passwordless, Passwordless)
+
+  unless Application.get_env(:passwordless, Passwordless) do
+    raise "Missing Passwordless configuration!"
+  end
+
   def schema,     do: get_config(:schema)
   def repo,       do: get_config(:repo)
   def mailer,     do: get_config(:mailer)
   def secret_key, do: get_config(:secret_key)
 
   defp get_config(name) do
-    if value = Keyword.get(get_config(), name) do
+    if value = Keyword.get(@config, name) do
       value
     else
       raise "Missing Passwordless configuration: #{name}"
     end
-  end
-
-  defp get_config do
-    Application.get_env(:passwordless, Passwordless)
   end
 end
